a81db8fe482910fbafa8584a1cee6fc1493b03de
LAPTOP-2Q1JFT7S\imimbora <imimbora@gmail.com>
2021-08-27 17:49:46 +0900

    add tab

--- src/views/dashboard/pages/TotalDashboardPage.vue
+++ src/views/dashboard/pages/TotalDashboardPage.vue
@@ -17,6 +17,25 @@
             </p-button>
         </div>
 
+        <!-- KB Domain Tab -->
+        <div class="domain-tab">
+            <ul>
+                <li><button class="btn active">KB국민은행</button></li>
+                <li><button class="btn">KB증권</button></li>
+                <li><button class="btn">KB손해보험</button></li>
+                <li><button class="btn">KB국민카드</button></li>
+                <li><button class="btn">푸르덴셜생명</button></li>
+                <li><button class="btn">KB자산운용</button></li>
+                <li><button class="btn">KB캐피탈</button></li>
+                <li><button class="btn">KB생명보험</button></li>
+                <li><button class="btn">KB부동산신탁</button></li>
+                <li><button class="btn">KB저축은행</button></li>
+                <li><button class="btn">KB인베스트먼트</button></li>
+                <li><button class="btn">KB데이타시스템</button></li>
+                <li><button class="btn">KB신용정보</button></li>
+            </ul>
+        </div>
+
         <!-- If you want to reload when the state is changed, bind key with reactive state. -->
         <div :key="test.toString()">
             <!-- Give extra parameter objects for api requests in widgets. -->
@@ -66,7 +85,43 @@ export default defineComponent({
 </script>
 
 <style lang="postcss" scoped>
-.total-dashboard {
+.total-dashboard::v-deep {
+    @apply bg-gray-100;
+    height: auto;
 }
-
+.domain-tab {
+    position: relative;
+    margin-bottom: 35px;
+}
+.domain-tab > ul {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -10px;
+}
+.domain-tab li {
+    width: 208px;
+    margin-top: 12px;
+    padding: 0 10px;
+}
+.domain-tab .btn {
+    overflow: hidden;
+    display: block;
+    width: 100%;
+    height: 41px;
+    padding: 0 20px;
+    text-align: center;
+    font-size: 15px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    background-color: #eee;
+    border-radius: 4px;
+}
+.domain-tab .btn:hover {
+    background-color: #e9e9e9;
+}
+.domain-tab .btn.active {
+    font-weight: bold;
+    color: #fff;
+    background-color: #6638B6;
+}
 </style>
